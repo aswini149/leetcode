7f0ffa3429f2fcaa6d9c549733733959b352a652
Aswini Rath <archu.aswini@gmail.com>
2026-02-02 09:59:26 +0100

    LC-reverse-vowels-of-a-string [Runtime: 12 ms; Memory: 46.5 MB]

---
+++ string/reverse-vowels-of-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s) {
+        Set<Character> set = new HashSet<>();
+        set.add('a');set.add('e');set.add('i');set.add('o');set.add('u');
+        set.add('A');set.add('E');set.add('I');set.add('O');set.add('U');
+        char[] arr = s.toCharArray();
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (!set.contains(arr[i])) {
+                i++;
+            } else if (!set.contains(arr[j])) {
+                j--;
+            } else {
+                // both are vowels → swap
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr) + " res");
+        return new String(arr);
+    }
+}
