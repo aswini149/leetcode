5ae8aa0142ae429f30097e35453bed4b0bd216b1
Aswini Rath <archu.aswini@gmail.com>
2026-02-03 09:13:29 +0100

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (map.containsKey(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (stack.peek() != map.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+            System.out.println(stack.toString());
+
+        }
+        return stack.isEmpty();
+    }
+}
