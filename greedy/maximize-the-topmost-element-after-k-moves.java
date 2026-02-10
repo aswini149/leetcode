b3bd7231446b79a60d1c5d0dee2b87444c5886b1
Aswini Rath <archu.aswini@gmail.com>
2026-02-10 08:55:32 +0100

    LC-maximize-the-topmost-element-after-k-moves [Runtime: N/A; Memory: N/A]

--- greedy/maximize-the-topmost-element-after-k-moves.java
+++ greedy/maximize-the-topmost-element-after-k-moves.java
@@ -2,7 +2,7 @@ class Solution {
     public int maximumTop(int[] nums, int k) {
         int max = -1;
         int i = 0;
-        while (i < k - 1) {
+        while (i < nums.length && i < k - 1) {
             max = Math.max(max, nums[i]);
             i++;
         }
