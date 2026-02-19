d4a0c7fa759d8ec7c95a682cb41cfccc43131871
Aswini Rath <archu.aswini@gmail.com>
2026-02-19 16:45:45 +0100

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: 0 ms; Memory: 48.4 MB]

---
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = searchFirst(nums, target);
+        int second = searchSecond(nums, target);
+
+        return new int[]{first, second};
+    }
+
+    private static int searchFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + (right - left ) / 2);
+            if (nums[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    private static int searchSecond(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + (right - left ) / 2);
+            if (nums[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
